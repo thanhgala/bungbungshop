22ba9ce5f81e25770f79d1ef4d845f32aa0af51b
DESKTOP-PMFMADO\Thanh <tabimtop@gmail.com>
2017-03-06 03:02:45 +0700

    Contruction and create model layer

---
+++ BungBungShop.Model/Abstrack/Auditable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BungBungShop.Model.Abstrack
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string CreatedBy { set; get; }
+
+        [MaxLength(256)]
+        public DateTime? UpdatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string UpdateBy { set; get; }
+
+        [MaxLength(256)]
+        public string MetaKeyword { set; get; }
+
+        [MaxLength(256)]
+        public string MetaDescription { set; get; }
+
+        public bool Status { set; get; }
+    }
+}
---
+++ BungBungShop.Model/Models/OrderDetail.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BungBungShop.Model.Models
+{
+    [Table("OrderDetails")]
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderID { set; get; }
+
+        [Key]
+        public int ProductID { set; get; }
+
+        public int Quantitty { set; get; }
+
+        [ForeignKey("OrderID")]
+        public virtual Order Order { set; get; }
+
+    }
+}
